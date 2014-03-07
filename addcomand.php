3384475c08d89e6564267349932fb5e474caaca8
Drakonkn <drakonkn@mail.ru>
2014-03-07 05:14:08 +0400

    add user id dev id dev name

--- addcomand.php
+++ addcomand.php
@@ -1,22 +1,17 @@
-		<?php
-		$link = mysql_connect('localhost', 'root', 'Drakowa')
-		    or die('Не удалось соединиться: ' . mysql_error());
-		echo 'Соединение успешно установлено';
-		mysql_select_db('command') or die('Не удалось выбрать базу данных/n');
+<?php
+	include 'db/db.php';
+	$dbname = 'command';
 
-		// Выполняем SQL-запрос
-		$cmd = $_POST['cmd'];
-		$source = 1;//$_POST['source'];
-		$dest = 2;//$_POST['dest'];
-
-
-		$query = 'SELECT * FROM commands';
-		$query = "INSERT INTO commands (cmd, source, dest) VALUES ('".$cmd."', '".$source."', '".$dest."')";
-		$result = mysql_query($query) or die('Запрос не удался: ' . mysql_error());
-
-		// Освобождаем память от результата
-		mysql_free_result($result);
-
-		// Закрываем соединение
-		mysql_close($link);
-		?>
+	$command_db = new db($dbname);
+	$command_db->init();
+	$cmd = $_POST['cmd'];
+	if (isset($_COOKIE['dev_id']))
+		$source = $_COOKIE['dev_id'];
+	else {
+		echo "error, please true again";
+		return;
+	}
+	$dest = 2;//$_POST['dest'];
+	$query = "INSERT INTO commands (cmd, source, dest) VALUES ('".$cmd."', '".$source."', '".$dest."')";
+	$command_db->request($query);
+?>
