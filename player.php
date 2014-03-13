612fce8b6a69b7fa827d6b6de1cc355041556589
Drakonkn <drakonkn@mail.ru>
2014-03-13 17:41:55 +0400

    deleting files new player in main

--- player.php
+++ player.php
@@ -2,35 +2,53 @@
 <html>
  <head>
  	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="main.css"/>
+	<link rel="stylesheet" type="text/css" href="player.css"/>
   	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-  	<script type="text/JavaScript" src="scripts.js"></script>
+  	<script type="text/javascript">
+  		</script>
   	<script type="text/JavaScript" src="dev_id_cheker.js"></script>
+  	<script type="text/JavaScript" src="player.js"></script>
   	<title>Music player</title>
 </head>
 <body>
-<?php session_start();
-	include "vkAPI/vk_API.php";
-	ini_set('display_errors','On');
-	$vk = vk::getInstance();
-	if(!isset($_SESSION['uid'])){
-		redirect("index.php");
-	}
-	$songs = $vk->api('audio.get');
-	echo '<div id="song_list">';
-	foreach ($songs as $key => $song) {
-		echo '<div onclick="play(this)" class="song_element" path="'.$song->url.'">'.$song->title.'</div>';
-	}
+<div id="main_wrap">
+	<div class="floating" id="main_head">
+		Аудио плеер
+		<div id="dev_info">
+			no info
+		</div>
+		<div id="audio_info">
+			no info
+		</div>
+	</div>
+	<div id="content">
+		<div class="text_container_l">
+			<div class="text_container_r">
+					<div id="right_content">
+						<div id="player_wraper">
+							<audio controls id="player">
+							  		<source id="src" src="" type="audio/mpeg">
+							  		Тег audio не поддерживается вашим браузером. 
+							</audio>
+						</div>
+							<div id="list_wraper">
+								
+							</div>
+					</div>
+					<div id="left_content">
+							<select id="devices" onchange="onOriginSet(this)">
+	  							<option selected="selected" value = "myAudio">Мои аудиозаписи</option>
+	  							<option value = "recomends">Рекомендации</option>
+							</select>
+					</div>
 
-	function redirect($url){
-		header('Location: '.$url);
-		exit;
-	}
-?>
- </div>
-  <audio controls id="player">
-    <source id="src" src="" type="audio/mpeg">
-    Тег audio не поддерживается вашим браузером. 
-  </audio>
+			</div>
+		</div>
+
+
+
+
+	</div>
+</div>
 </body>
 </html>
